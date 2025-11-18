b777ad601b23ff37e72e92d8fdff4f80c14bfee6
SohailaN12 <sohaila.noori2027@gbwl.org>
2025-11-18 16:06:27 +0000

    2.8 notes

--- Main.java
+++ Main.java
@@ -19,6 +19,31 @@ public class Main {
          count++;
       }
 
+      // FOR LOOPA: when you know HOW MANY TIMES to ilerate
+      // for (initialize; test condition; change)
+      for ( int i = 5; i <= 27; i++) {
+         System.out.println(i);
+      }
+      // Modify the CHANGE step to skip certain numbers
+      for (int i = 0; i <= 10; i+=2 ) {
+         System.out.println(i);
+      }
+      // Watch your INITIAL values and your OPERATOR
+      for (int i = 0; i < 12; i+=2) {
+         System.out.println(i);
+      }
+      // this loop did the SAME THING as the loop above it!
+
+      // DECREMENTING for loops are like counting backwards
+      for ( int i = 10; i > 0; i--) {
+         System.out.println(i);
+         // Print a special meddage when i is 1 
+         if ( i == 1) {
+            System.out.println("Blast off!");
+         }
+      }
+
+
 
    } // END MAIN MATHOD
 } // END CLASS 
